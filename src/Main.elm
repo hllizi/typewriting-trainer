596b6dd7b12018e967fd2c9bc212cd20b8b887aa
dlahm <david@klinger-lahm.de>
2021-01-02 18:37:05 +0100

    time intervals fixed, coloring mistakes.

--- src/Main.elm
+++ src/Main.elm
@@ -2,12 +2,15 @@ module Main exposing (..)
 
 import Browser
 import Browser.Events exposing (onKeyPress)
-import Html exposing (Html, button, div, h1, text)
+import Html exposing (Html, Attribute, button, div, h1, text)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 import Random
 import Time
 import List exposing (head)
 import Json.Decode exposing (field, string, map)
+import Task exposing (perform)
+import Time exposing (millisToPosix)
 
 main = Browser.element {
     init = init, 
@@ -15,38 +18,53 @@ main = Browser.element {
     subscriptions = subscriptions, 
     view = view}
 
-type Msg = Change | CorrectKeyPressed Char | IncorrectKeyPressed | NewChar Char
+type Msg = Change | CorrectKeyPressed Char | IncorrectKeyPressed | NewChar Char | SetLastUpdateTime Time.Posix | Pass
 type alias Model = 
     {
+        interval: Int,
+        subIntervalLength: Int,
         character: Char,
-        mistakeMade: Bool
+        mistakeMade: Bool,
+        lastUpdateTime: Time.Posix
         }
 
 init : () -> (Model, Cmd Msg)
-init _ = (Model 'A' False, Cmd.none)
+init _ = (Model 1000 100 'A' False (millisToPosix 0), readTime)
 
-timedChange : Time.Posix -> Msg
-timedChange _ = Change
+timedChange : Int -> Time.Posix -> Time.Posix -> Msg
+timedChange factor last now = 
+    if Time.posixToMillis last < Time.posixToMillis now - (factor * 100)  
+    then 
+        Change
+    else
+        Pass
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
     case msg of 
-        CorrectKeyPressed _ -> ({model | character = 'Ö'}, Cmd.none) 
+        CorrectKeyPressed _ -> update Change model
         IncorrectKeyPressed -> ({model | mistakeMade = True }, Cmd.none)
         Change ->
                     (
-                        model, 
-                        Random.generate 
-                        (NewChar << Char.fromCode) 
-                        (Random.int 
-                        (Char.toCode 'A') 
-                        (Char.toCode 'z'))
-                        )
+                        {model | mistakeMade = False}, 
+                        Cmd.batch [ 
+                            Random.generate 
+                            (NewChar << Char.fromCode) 
+                            (Random.int 
+                            (Char.toCode 'A') 
+                            (Char.toCode 'z')),
+                            readTime]
+                            )
+        SetLastUpdateTime time -> ({model | lastUpdateTime = time}, Cmd.none)
         NewChar c -> ({model | character = c}, Cmd.none)
+        Pass -> (model, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.batch [
-    Time.every 1000 timedChange,
+    let subIntervalNumber = model.interval // model.subIntervalLength
+    in
+     Time.every (toFloat model.subIntervalLength) (timedChange subIntervalNumber model.lastUpdateTime)
+     ,
     onKeyPress (map (checkCharacter model << toMaybeChar) <| field "key" string)
     ]
 
@@ -69,13 +87,20 @@ checkCharacter model maybeC =
         CorrectKeyPressed model.character 
     else
         IncorrectKeyPressed
-            
 
+readTime: Cmd Msg
+readTime = perform SetLastUpdateTime Time.now 
+
+coloring: Bool -> List(Attribute msg)
+coloring mistakeMade = 
+    if mistakeMade 
+    then [style "color" "red"]
+    else []
 
 view: Model -> Html Msg
 view model = 
     div []
-    [ h1 [] [ text  (String.fromChar model.character ) ] ]
+    [ h1 (coloring model.mistakeMade) [ text  (String.fromChar model.character ) ] ]
 
 
 
